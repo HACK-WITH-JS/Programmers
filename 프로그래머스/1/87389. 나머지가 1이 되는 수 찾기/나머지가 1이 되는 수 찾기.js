778f6701c887841fedbeab46d94bc2fd4c54c494
HACK-WITH-JS <154941658+HACK-WITH-JS@users.noreply.github.com>
2024-01-09 07:37:11 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = 2;
+    
+    while(true) {
+        if(n % answer === 1) {
+            break;
+        }
+        
+        answer++;
+    }
+    
+    return answer;
+}
