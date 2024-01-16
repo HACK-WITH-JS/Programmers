9849ac5433bc17c32a509ab7c2919d2d93027d4c
Hack <154941658+HACK-WITH-JS@users.noreply.github.com>
2024-01-16 10:48:44 +0900

    [level Lv.1] Title: 같은 숫자는 싫어, Time: 45.34 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,19 @@
+function solution(arr)
+{
+    const answer = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(i === 0) {
+            answer.push(arr[i]);
+            continue;
+        }
+        
+        if(answer[answer.length-1] === arr[i]) {
+            continue;
+        }
+        
+        answer.push(arr[i]);
+    }
+    
+    return answer;
+}
